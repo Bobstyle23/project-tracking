7fe3fb1246d0680a1a8f7f30d7e5508dc5f65c29
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-03-13 16:34:28 +0900

    Change some styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const menuOpenBtn = document.querySelector(".icon__hamburger");
 const menuCloseBtn = document.querySelector(".icon__close");
 const menuList = document.querySelector(".navigation__list");
+const menuItems = menuList.children;
 
 const toggleMenu = () => {
   menuOpenBtn.classList.toggle("hidden");
@@ -20,3 +21,12 @@ menuCloseBtn.addEventListener("click", () => {
   toggleMenu();
   toggleList("-100rem");
 });
+
+for (let i = 0; i < menuItems.length; i++) {
+  const element = menuItems[i];
+
+  element.addEventListener("click", (e) => {
+    toggleList("-100rem");
+    toggleMenu();
+  });
+}
